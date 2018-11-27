3f57c27d5aaa85ff78bcdd57b8ebece11501cec0
Nathan Burlis <43301944+burlisn@users.noreply.github.com>
2018-11-26 18:19:55 -0600

    Update hw10.cpp

--- hw10/hw10.cpp
+++ hw10/hw10.cpp
@@ -1,4 +1,9 @@
-//
+// Programmer: Nathan Burlis & Mathew Romine
+// Date: 12/05/2018
+// Filename: hw10.cpp
+// Course number & section: 1570 C
+// Instructor name: Yeung
+// Purpose:
 
 int main()
 {
